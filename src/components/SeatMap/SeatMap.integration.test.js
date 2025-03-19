608cb61566216a1e0b47088bf5410e5a002ad828
mar <109629047+marlibs3@users.noreply.github.com>
2025-03-19 12:11:42 +0000

    SeatMap Integration Tests (#50)
    * :white_check_mark: adding seatmap integration test for tooltip.
    
    * :construction: wip fixing storybook.
    
    * :white_check_mark: adding seatmap integration test for seat selection happy path.
    
    * :white_check_mark: adding seatmap integration test for seat deselection happy path.
    
    * :construction: wip untangling fixtures.
    
    * :fire: deleting wip test.
    
    * :rewind: reverting commits that have been passed to new branch.
    
    * :sparkles: addressing pr comments.
    
    * :pushpin: keeping package.json to main version.
    
    * :pushpin: keeping package.json to version that makes no diffs...
    
    ---------
    
    Co-authored-by: Jas Vadgama <jasal.vadgama@travelperk.com>
    Co-authored-by: Raul Mozo <105198685+raul-travelperk@users.noreply.github.com>
    Co-authored-by: lindybritstravelperk <138686105+lindybritstravelperk@users.noreply.github.com>
    Co-authored-by: Tony Gallagher <tony.gallagher@travelperk.com>

---
+++ src/components/SeatMap/SeatMap.integration.test.js
@@ -0,0 +1,229 @@
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import { JetsSeatMap } from './SeatMap';
+import { flightDetails } from './__fixtures__/seatMapApiGetPlaneFeatures';
+import { CONFIG_MOCK } from '../Demo/constants';
+import { cabin, entertainment, power, seatDetails, wifi } from './__fixtures__/seatMapApiPostDataResponse';
+
+const setup = ({
+  flight,
+  availability,
+  passengers,
+  currentDeckIndex,
+  configOverrides,
+  onSeatMapInited,
+  onAvailabilityApplied,
+}) => {
+  const config = {
+    ...CONFIG_MOCK,
+    ...configOverrides,
+  };
+  return render(
+    <JetsSeatMap
+      flight={flight}
+      availability={availability}
+      passengers={passengers}
+      currentDeckIndex={currentDeckIndex}
+      config={config}
+      onSeatMapInited={onSeatMapInited}
+      onAvailabilityApplied={onAvailabilityApplied}
+    />
+  );
+};
+
+const mockPostData = jest.fn();
+
+jest.mock('./api', () => {
+  const module = jest.requireActual('./api');
+  return {
+    ...module,
+    JetsSeatMapApiService: class extends module.JetsSeatMapApiService {
+      postData = mockPostData;
+    },
+  };
+});
+
+describe('JetsSeatMap', () => {
+  it('should show the tooltip with the correct data when a seat is clicked', async () => {
+    const flight = flightDetails();
+
+    const singleCabinResponseFixture = [
+      {
+        id: '1111',
+        cabin: cabin(),
+        entertainment: entertainment(),
+        power: power(),
+        wifi: wifi(),
+        seatDetails: seatDetails(),
+      },
+    ];
+    mockPostData.mockImplementation(() => singleCabinResponseFixture);
+
+    setup({
+      flight: flight,
+      availability: null,
+      passengers: null,
+      currentDeckIndex: 0,
+    });
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByText(/33A/));
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText(/Premium Economy 33A/)).toBeInTheDocument();
+      expect(screen.getByText(/Select/)).toBeInTheDocument();
+    });
+  });
+
+  it('should trigger onSeatSelected when a passenger selects an available seat', async () => {
+    const flight = flightDetails();
+
+    const singleCabinResponseFixture = [
+      {
+        id: '1111',
+        cabin: cabin(),
+        entertainment: entertainment(),
+        power: power(),
+        wifi: wifi(),
+        seatDetails: seatDetails(),
+      },
+    ];
+
+    mockPostData.mockImplementation(() => singleCabinResponseFixture);
+
+    const passengers = [
+      {
+        id: '1',
+        seat: null,
+        passengerLabel: 'John Doe',
+        passengerColor: 'brown',
+        readOnly: false,
+      },
+    ];
+
+    const availability = [
+      {
+        currency: 'EUR',
+        label: '33A',
+        price: 0,
+      },
+    ];
+
+    const onSeatSelected = jest.fn();
+
+    const { rerender } = setup({
+      flight,
+      availability: null,
+      passengers: null,
+      currentDeckIndex: 0,
+    });
+
+    rerender(
+      <JetsSeatMap
+        flight={flight}
+        passengers={passengers}
+        availability={availability}
+        currentDeckIndex={1}
+        onSeatSelected={onSeatSelected}
+      />
+    );
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByText(/33A/));
+    });
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByText(/Select/));
+    });
+
+    expect(onSeatSelected).toHaveBeenCalledTimes(1);
+    expect(onSeatSelected).toHaveBeenCalledWith([
+      {
+        abbr: 'JD',
+        id: '1',
+        passengerColor: 'brown',
+        passengerLabel: 'John Doe',
+        readOnly: false,
+        seat: { price: undefined, seatLabel: '33A' },
+      },
+    ]);
+  });
+
+  it('should trigger onSeatUnselected when a passenger unselects a seat they just selected', async () => {
+    const flight = flightDetails();
+
+    const singleCabinResponseFixture = [
+      {
+        id: '1111',
+        cabin: cabin(),
+        entertainment: entertainment(),
+        power: power(),
+        wifi: wifi(),
+        seatDetails: seatDetails(),
+      },
+    ];
+
+    mockPostData.mockImplementation(() => singleCabinResponseFixture);
+
+    const passengers = [
+      {
+        id: '1',
+        seat: null,
+        passengerLabel: 'John Doe',
+        passengerColor: 'brown',
+        readOnly: false,
+      },
+    ];
+
+    const availability = [
+      {
+        currency: 'EUR',
+        label: '33A',
+        price: 0,
+      },
+    ];
+
+    const onSeatSelected = jest.fn();
+    const onSeatUnselected = jest.fn();
+
+    const { rerender } = setup({
+      flight,
+      availability: null,
+      passengers: null,
+      currentDeckIndex: 0,
+    });
+
+    rerender(
+      <JetsSeatMap
+        flight={flight}
+        passengers={passengers}
+        availability={availability}
+        currentDeckIndex={1}
+        onSeatSelected={onSeatSelected}
+        onSeatUnselected={onSeatUnselected}
+      />
+    );
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByText(/33A/));
+    });
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByText(/Select/));
+    });
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByText(/33A/));
+    });
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByText(/Unselect/));
+    });
+
+    expect(onSeatUnselected).toHaveBeenCalledTimes(1);
+    expect(onSeatUnselected).toHaveBeenCalledWith([
+      { abbr: 'JD', id: '1', passengerColor: 'brown', passengerLabel: 'John Doe', readOnly: false, seat: null },
+    ]);
+  });
+});
