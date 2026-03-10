b8d966c70ed75f53db42977ec22e71f13586813e
Iurii Perevertailo <the.one.nexus@gmail.com>
2026-03-10 17:08:05 +0100

    React 19 compatible (#149)
    * React 19 compatibility;

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './Button.css';
 
-export const JetsButton = ({ content, onClick, className, disabled, active, ...attrs }) => {
+export const JetsButton = ({
+  content = 'Btn',
+  onClick = () => {},
+  className = 'jets-btn',
+  disabled = false,
+  active = false,
+  ...attrs
+}) => {
   return (
     <button {...attrs} className={className} onClick={onClick} disabled={disabled}>
       {content}
     </button>
   );
-};
-
-JetsButton.defaultProps = {
-  content: 'Btn',
-  className: 'jets-btn',
-  disabled: false,
-  active: false,
-  onClick: () => {},
 };
